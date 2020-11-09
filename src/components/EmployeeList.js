8f390db3fdf88680d98d5368d911c91ae506e89a
Arin <arinyazilim@gmail.com>
2020-11-09 16:47:35 +0300

    Ders 10

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,12 +1,31 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Employee from './Employee';
+import { Button, Modal } from 'react-bootstrap';
 import { EmployeeContext } from '../contexts/EmployeeContext';
+import AddForm from './AddForm';
 
 const EmployeeList = () => {
 
     const {employees} = useContext(EmployeeContext)
 
+    const [show, setShow] = useState(false)
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true)
+
     return (
+
+        <>
+        <div className="table-title">
+        <div className="row">
+          <div className="col-sm-6">
+            <h2>Manage <b>Employees</b></h2>
+          </div>
+          <div className="col-sm-6">
+            <Button onClick={handleShow} className="btn btn-success text-white" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New Employee</span></Button>
+          </div>
+        </div>
+      </div>
 
         <table className="table table-striped table-hover">
             <thead>
@@ -22,6 +41,23 @@ const EmployeeList = () => {
                 <Employee employees={employees}/>
             </tbody>
         </table>
+
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header className="modal-header" closeButton>
+                <Modal.Title>
+                    Add Employee
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <AddForm />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close Modal
+                </Button>
+            </Modal.Footer>
+        </Modal>
+        </>
     )
 }
 
